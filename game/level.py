1fa687f37ee97117d2ff80431aa2b6612b6a4744
robbiebmorris <77032126+robbiebmorris@users.noreply.github.com>
2022-07-13 01:26:09 +0300

    player

--- game/level.py
+++ game/level.py
@@ -1,21 +1,29 @@
-import pygame 
+import pygame
 from settings import *
 from tile import *
+from player import *
+
+
 class Level:
-	def __init__(self):
+    def __init__(self):
 
-		self.display_surface = pygame.display.get_surface()
-		self.visible_sprites = pygame.sprite.Group()
-		self.obstacle_sprites = pygame.sprite.Group()
-		self.create_map()
+        self.display_surface = pygame.display.get_surface()
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
+        self.create_map()
 
-	def create_map(self):
-		for row_index,row in enumerate(WORLD_MAP):
-			for col_index, col in enumerate(row):
-				x = col_index * TILESIZE
-				y = row_index * TILESIZE
-				if col == 'x':
-					Tile((x,y),[self.visible_sprites],'game/graphics/test/waves.png')
-	def run(self):
-		# update and draw the game
-		self.visible_sprites.draw(self.display_surface)
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites,
+                         self.obstacle_sprites], "graphics/test/rock.png")
+                if col == 'p':
+                    self.player = Player((x, y), [self.visible_sprites])
+
+    def run(self):
+        # update and draw the game
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
