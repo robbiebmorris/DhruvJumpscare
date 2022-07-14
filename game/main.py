30a869612b01c633e4d647d871ed9afa9d4a1dad
codercreator <sean.choi2004@gmail.com>
2022-07-14 12:59:30 +0800

    useless

--- game/main.py
+++ game/main.py
@@ -11,14 +11,14 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
-        self.backpack = Button(20, 636, "game/graphics/test/backpack.png")
-        self.map = Button(94, 636, "game/graphics/test/map.png")
-        self.spellbook = Button(168, 636, "game/graphics/test/spellbook.png")
-        self.settings = Button(242, 636, "game/graphics/test/settings.png")
         self.bpos_x = 0
         self.bpos_y = 0
         self.background = pygame.image.load("game/graphics/tilemap/ground.png")
         self.screen.blit(self.background,(0,0))
+        self.backpack = Button(20, 636, "game/graphics/test/backpack.png")
+        self.map = Button(94, 636, "game/graphics/test/map.png")
+        self.spellbook = Button(168, 636, "game/graphics/test/spellbook.png")
+        self.settings = Button(242, 636, "game/graphics/test/settings.png")
         pygame.display.set_caption('Dhruv Jumpscare')
         self.clock = pygame.time.Clock()
         self.level = Level()
